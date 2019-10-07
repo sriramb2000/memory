d6f77c3a3b241925cf5b59defa838f6a525dd73d
Sriram Balachandran <sriramb2000@gmail.com>
2019-10-06 21:46:00 -0700

    test: add boilerplate test code

---
+++ test/card/__init__.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("../../")
+import unittest
+
+import src.logic.card
+
+class TestCard(unittest.TestCase):
+    def test_is_pair(self):
+        card1 = Card()
+        card2 = Card()
+        self.assertTrue()
